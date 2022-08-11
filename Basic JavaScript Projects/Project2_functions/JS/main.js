e3de92c9392196bcedc8bf6716616e996dd42ca6
aryanaamodio <108484511+aryanaamodio@users.noreply.github.com>
2022-08-11 14:02:16 -0400

    Initial commit

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+function My_First_Function(){//Here is a function
+    var str = "This is the button text!";
+    document.getElementById("Button_Text").innerHTML=str;//my first function
+
+}
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence +="a lot from this book!";
+    document.getElementById("Concatenate").innerHTML=sentence;//funtion that uses the operator
+}
